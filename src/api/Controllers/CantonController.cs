356b0a4cfd7d031b77d4757e5409c893f7211a3e
Philipp Luethi <philipp.luethi@geowerkstatt.ch>
2023-01-12 18:47:49 +0100

    Add CantonController

---
+++ src/api/Controllers/CantonController.cs
@@ -0,0 +1,30 @@
+﻿using BDMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace BDMS.Controllers;
+public class CantonController : Controller
+{
+    private readonly BdmsContext context;
+
+    public CantonController(BdmsContext context)
+    {
+        this.context = context;
+    }
+
+    /// <summary>
+    /// Asynchronously gets all existing values (except null) for <see cref="Borehole.Canton"/>/>.
+    /// </summary>
+    [HttpGet]
+    public async Task<ICollection<string?>> GetAllAsync()
+    {
+        var cantons = context.Boreholes
+            .Select(bho => bho.Canton)
+            .Where(c => c != null)
+            .Distinct()
+            .AsQueryable();
+
+        return await cantons.ToArrayAsync().ConfigureAwait(false);
+    }
+}
